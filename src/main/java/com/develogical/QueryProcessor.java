691cd695d5d75e4e46d82d0f32abc31d788329c7
Alice Nie <alicenie.02@gmail.com>
2022-09-22 16:06:48 -0400

    Largest num

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,4 +1,5 @@
 package com.develogical;
+import java.lang.*;
 
 public class QueryProcessor {
 
@@ -13,6 +14,26 @@ public class QueryProcessor {
         }
         if (query.toLowerCase().contains("what is your name")) {
             return "ASA";
+        }
+
+        if (query.toLowerCase().contains("which of the following numbers is the largest")) {
+
+            String[] parts = input.replace(",", " ");
+            parts = input.replace(":", " ");
+            parts = input.split(" ");
+
+            Int[] int_array = new int[parts.length];
+            int index = 0;
+            for (int i=parts.length-1; i < parts.length; i--){
+                try {
+                    int num = Integer.parseInt(parts[i])
+                    int_array[index] = num;
+                    index++;
+                }
+                catch (NumberFormatException e){
+                    break;
+                }
+            return Math.max(int_array);
         }
         return "";
     }
